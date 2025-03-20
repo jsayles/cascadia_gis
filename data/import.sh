1143541f972ce8d8253392eb857d6f7587824fa8
Jacob Sayles <jsayles@gmail.com>
2025-03-20 10:54:47 -0700

    found more data files

--- data/import.sh
+++ data/import.sh
@@ -1,1 +1,1 @@
-ogr2ogr -f "PostgreSQL" PG:"dbname=cascadia_gis user=postgres" cities/ne_10m_populated_places.shp -nln cascadia_city -append
+ogr2ogr -f "PostgreSQL" PG:"dbname=cascadia_gis user=postgres" cities.shp -nln cascadia_city -append
