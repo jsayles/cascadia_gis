9631e427997aa80a09ab969e788a7818caccd76a
Jacob Sayles <jsayles@gmail.com>
2025-03-21 09:57:17 -0700

    hardcode main cities in map to start

--- cascadia/views.py
+++ cascadia/views.py
@@ -6,11 +6,19 @@ from cascadia.models import Map, City
 
 def home(request):
     map = Map.objects.first()
-    cities = City.objects.all()
+
+    # cities = City.objects.all()
+    vancouver = City.objects.get(id=7256)
+    seattle = City.objects.get(id=7109)
+    portland = City.objects.get(id=6782)
+    cities = [vancouver, seattle, portland]
 
     context = {
         "map": map,
         "cities": cities,
+        "vancouver": vancouver,
+        "seattle": seattle,
+        "portland": portland,
     }
     return render(request, "cascadia/home.html", context)
 
